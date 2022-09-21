f7fd203de55269024243b16355a9718417057f6e
Jorge Dueñas Lerín <jorge.duenas.lerin@gmail.com>
2022-09-21 21:01:13 +0200

    Práctica5++

--- ut01-introducción/03-ejercicios/practica5.php
+++ ut01-introducción/03-ejercicios/practica5.php
@@ -1,6 +1,17 @@
 <?php 
 $nombre='Jorge';
-$r = 3;
+$r = 0;
+$error = false;
+// $_GET Información de la cabecera
+if (isset($_GET['radio'])) {
+    $r = $_GET['radio'];
+    if ($r == "") {
+        $r = 0;
+        $error = true;
+    }
+} else {
+    $r = 0;
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,8 +24,17 @@ $r = 3;
 <body>
     <h1>Bienvenido a la calculadora <?=$nombre?></h1>
     <div>
-        <p>Área: <?=$r*$r*M_PI?></p>
-        <p>Longitud: <?=2*$r*M_PI?></p>
+        <?php if ($error) { ?>
+            <h3>Eres un poco manazas. Mete un número</h3>
+        <?php } ?>
+        <form action="practica5.php" method="get">
+            Radio: <input type="number" step="0.001" name="radio" id="" value="<?=$r?>"><br>
+            <input type="submit" value="Calcular">
+        </form>
+    </div>
+    <div>
+        <p>Área: <?= (1*$r*$r*M_PI) ?></p>
+        <p>Longitud: <?= (2*$r*M_PI)?></p>
     </div>
 </body>
 </html>
