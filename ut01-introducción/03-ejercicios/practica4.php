5901e0a310c29ec31ce5c94416cec1bc96e4efc4
Jorge Dueñas Lerín <jorge.duenas.lerin@gmail.com>
2022-09-21 20:18:03 +0200

    Práctica 4

---
+++ ut01-introducción/03-ejercicios/practica4.php
@@ -0,0 +1,17 @@
+<?php
+$nombre='Jorge';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilo4.css">
+</head>
+<body>
+    <h1>¡Hola mundo!</h1>
+    <p>Esta página ha sido progrmada por <span><?=$nombre?></span></p>
+</body>
+</html>
