30186480ba76b0fcffcc5097e74122968d4b12b7
Jorge Dueñas Lerín <jorge.duenas.lerin@gmail.com>
2022-11-02 21:45:18 +0100

    Avances en ejemplo completo AJAX

--- ut04-aplicaciones-web/Formularios/10BonusAjax/js/forms.js
+++ ut04-aplicaciones-web/Formularios/10BonusAjax/js/forms.js
@@ -1,43 +1,38 @@
+let valorAJAXPost = document.getElementById('post-form-valor')
+let button = document.getElementById('enviar')
+let loading = document.getElementById('loading')
 
-var valorAJAXGet = document.getElementById('get-form-valor');
-var valorAJAXPost = document.getElementById('post-form-valor');
-
-var contador = document.getElementById('contador');
+let contador = document.getElementById('contador')
 
 setInterval(function(){
   contador.innerText = 1 + parseInt(contador.innerText)
 }, 1000);
 
-document.getElementById('get-form').onsubmit = function() {
+
+document.getElementById('post-form').onsubmit = function() {
     var xhttp = new XMLHttpRequest();
+    button.style.display="none";
+    loading.style.display="inline";
+
     // open(method, url, async)
-    xhttp.open("GET", "http://127.0.0.1:8080?valor=" + valorAJAXGet.value, true);
-    xhttp.send();
-    // No enviamos el formulario normal
-    // hemos enviado la información por AJAX
-    /*
+    xhttp.open("POST", "http://127.0.0.1:8080/backend.php", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("valor=" + valorAJAXPost.value);
+
     xhttp.onreadystatechange = function() {//Call a function when the state changes.
         if(xhttp.readyState == 4 && xhttp.status == 200) {
-            alert(http.responseText);
+            alert(xhttp.responseText);
         }
+        loading.style.display="none";
+        button.style.display="inline";
         //  readyStates 
         // 0 	UNSENT 	Client has been created. open() not called yet.
         // 1 	OPENED 	open() has been called.
         // 2 	HEADERS_RECEIVED 	send() has been called, and headers and status are available.
         // 3 	LOADING 	Downloading; responseText holds partial data.
         // 4 	DONE 	The operation is complete.
-        
     }
-    */
-    
-    return false;
-};
 
-document.getElementById('post-form').onsubmit = function() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "http://127.0.0.1:8080", true);
-    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.send("valor=" + valorAJAXGet.value);
     // No enviamos el formulario normal
     // hemos enviado la información por AJAX
     return false;
